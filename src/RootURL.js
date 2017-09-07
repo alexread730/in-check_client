ea291b7f2b8686acfd886e68232af6c15eebb605
Alex Read <alexread730@gmail.com>
2017-09-07 14:40:54 -0600

    added index and app components

---
+++ src/RootURL.js
@@ -0,0 +1,1 @@
+export const ROOT_URL = (window.location.hostname === "localhost") ? `http://localhost:3000` : `https://knetic.herokuapp.com/`;
---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import {RootURL} from '../RootURL';
+
+class App extends Component {
+
+
+  render() {
+    return (
+      <div>
+        <p>inCheck!</p>
+      </div>
+    );
+  }
+}
+
+export default App;
