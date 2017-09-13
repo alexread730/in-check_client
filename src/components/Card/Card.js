a9a871b58857c0b5913f41af749313a493ee2731
Alex Read <alexread730@gmail.com>
2017-09-13 16:21:15 -0600

    cards formatted

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { Button, Icon } from 'semantic-ui-react'
 import './Card.css';
 
 export default class Card extends Component {
@@ -15,6 +16,12 @@ export default class Card extends Component {
           <h2 className="term">{this.props.details.term}</h2>
           <p className="def">{this.props.details.definition}</p>
           <p className="hint">Hint: {this.props.details.hint}</p>
+          <Button icon color={"teal"} size="mini">
+            <Icon name='edit' />
+          </Button>
+          <Button icon size="mini" color={"red"}>
+            <Icon name='delete' />
+          </Button>
         </div>
     )
   }
