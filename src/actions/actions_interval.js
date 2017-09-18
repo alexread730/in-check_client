c84be7a818e4c6471ea862f3d2c7e7f536bc8a3c
Alex Read <alexread730@gmail.com>
2017-09-18 13:04:36 -0600

    idk

--- src/actions/actions_interval.js
+++ src/actions/actions_interval.js
@@ -5,12 +5,12 @@ const url = 'http://localhost:3000/api/v1/accounts/1/decks'
 export const UPDATE_INTERVAL = 'UPDATE_INTERVAL';
 export const EDIT_DECK = 'EDIT_DECK';
 export const UPDATE_DECK_FORM = 'UPDATE_DECK_FORM';
+export const RESET_FORM = 'RESET_FORM';
 
 export function updateDeckInterval(id, interval) {
   const request = axios.put(`${url}/${id}`, {
     "interval": interval
   });
-
   return {
     type: UPDATE_INTERVAL,
     payload: request
@@ -18,9 +18,7 @@ export function updateDeckInterval(id, interval) {
 }
 
 export function editDeckInfo(props) {
-console.log(props);
   const request = axios.put(`${url}/${props.deck_id}`, props);
-
   return {
     type: EDIT_DECK,
     payload: request
@@ -36,3 +34,17 @@ export const changeDeckForm = ({property}, value) => {
     }
   };
 };
+
+export const resetForm = () => {
+  return {
+    type: RESET_FORM,
+    payload: {
+      deck_id: 0,
+      deckName: '',
+      deckDesc: '',
+      startTime: 0,
+      endTime: 23,
+      deckDays: []
+    }
+  }
+}
