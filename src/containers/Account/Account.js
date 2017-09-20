1052cfbc53baf8e26645984e5f65d5b1af038432
Alex Read <alexread730@gmail.com>
2017-09-20 00:20:37 -0600

    added dummy account data

---
+++ src/containers/Account/Account.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import { Icon } from 'semantic-ui-react'
+import './Account.css';
+
+
+export default class DeckDetail extends Component {
+
+  constructor(props) {
+    super();
+
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <section className='group'>
+          <Icon name="edit" size="large"/>
+          <h1>Alex Read</h1>
+        </section>
+        <section className='group'>
+          <Icon name="edit" size="large"/>
+          <h2>alexread730@gmail.com</h2>
+        </section>
+        <section className='group'>
+          <Icon name="edit" size="large"/>
+          <h2>4147588814</h2>
+        </section>
+      </div>
+
+    )
+
+  }
+}
