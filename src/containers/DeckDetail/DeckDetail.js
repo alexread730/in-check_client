7c7871797c1b1355456ec90c3777112032b93ff5
Alex Read <alexread730@gmail.com>
2017-09-20 00:00:28 -0600

    working add decks

--- src/containers/DeckDetail/DeckDetail.js
+++ src/containers/DeckDetail/DeckDetail.js
@@ -56,6 +56,7 @@ class DeckDetail extends Component {
           return (
             <div className="container">
               <section>
+                {console.log(this.props.deckInfo)}
                 <h1 className="title">{this.props.deckInfo[0][0].name}</h1>
                 <section className="deck-details">
                   <h3>Cards Completed: {this.state.numCompleted}/{this.props.cards[0].length}</h3>
