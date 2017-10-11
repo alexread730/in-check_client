f0c210ffbab3acd521d9f88c1fee56c197edcadf
Alex Read <alexread730@gmail.com>
2017-10-11 10:26:25 -0400

    logout button still not showing up

--- src/containers/Account/Account.js
+++ src/containers/Account/Account.js
@@ -9,6 +9,7 @@ import { Icon, Button } from 'semantic-ui-react'
 import './Account.css';
 
 //need to figure out why logout button isn't on deployed version
+//still none
 
 class Account extends Component {
 
